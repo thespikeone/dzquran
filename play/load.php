b42eec16eb859f68ba41be4c2b877b11f6a18899
THESPIKEONE <ysarni42@gmail.com>
2022-03-22 12:38:43 +0100

    v1.0

--- play/load.php
+++ play/load.php
@@ -5,5 +5,5 @@ $url="https://api.quran.com/api/v4/chapter_recitations/$id/$ids";
 $json = file_get_contents($url);
 $data_audio = json_decode($json, TRUE);
 $data_audio = $data_audio['audio_file'];
-var_dump($data_audio);
+
 ?>
